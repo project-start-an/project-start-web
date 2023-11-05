a2378c507b1033525b067fb65a1675f5622ec49d
Angel <chernaevangel@gmail.com>
2023-11-05 17:02:15 +0100

    MnContWrapper element export fixed

--- src/pages/styled-components/MnContWrapper.tsx
+++ src/pages/styled-components/MnContWrapper.tsx
@@ -7,15 +7,4 @@ const MnContWrapper = {
   backgroundColor: "grey",
 };
 
-// Define media query for desktop (e.g., screen width greater than 768px)
-const desktopMediaQuery = "@media (min-width: 769px)";
-
-// Add styles for desktop layout
-const desktopStyles = {
-  flexDirection: "row",
-};
-
-export default {
-  ...MnContWrapper, // Merge the common styles
-  [desktopMediaQuery]: desktopStyles, // Apply desktop styles within the media query
-};
+export default MnContWrapper;
